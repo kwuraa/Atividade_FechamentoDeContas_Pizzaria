64e4df6b344bbfdd7f08c739e7cbef79ac399464
kwuraa <kwuraa@gmail.com>
2026-09-22 21:06:54 -0300

    first commit

---
+++ Grupo_2_Exerc_01.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	// definição de variáveis
+
+	float price_chopp = 18.00, price_pizzaG = 60.00, price_cobert = 15.00, price_refri360ml = 12.00, price_water = 10.00, value_pessoa = 0, total = 0, porC_gorjeta = 0.08, value_consum = 0;
+	int qtd_pess, qtd_chopp, qtd_pizza, qtd_cobertura, qtd_refri, qtd_water;
+
+	char pizzariaName[50] = "????????????????";
+
+	return 0;
+}
